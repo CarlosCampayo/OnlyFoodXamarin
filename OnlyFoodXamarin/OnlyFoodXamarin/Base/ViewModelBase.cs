da4717e3f7457cf9142f21ec2338e22130f32e85
ccampayo <carlos.campayo@tajamar365.com>
2021-05-26 18:07:21 +0200

    Metida carperta base, con base de viewmodel

---
+++ OnlyFoodXamarin/OnlyFoodXamarin/Base/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace OnlyFoodXamarin.Base
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
