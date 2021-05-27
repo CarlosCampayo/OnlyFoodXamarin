244efd4f08678a663ec63b31bf886c345c61d61d
Gerard <gerard.np@outlook.es>
2021-05-27 22:24:42 +0200

    Paginas de cadenas y ofertas creadas con navegacion

---
+++ OnlyFoodXamarin/OnlyFoodXamarin/ViewModels/CadenasViewModel.cs
@@ -0,0 +1,75 @@
+﻿using OnlyFoodXamarin.Base;
+using OnlyFoodXamarin.Models;
+using OnlyFoodXamarin.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace OnlyFoodXamarin.ViewModels
+{
+    public class CadenasViewModel : ViewModelBase
+    {
+        public CadenasViewModel()
+        {
+            List<Cadena> cadenasLocal = new List<Cadena>()
+            {
+                new Cadena()
+                {
+                    Id = 1,
+                    Nombre = "Burguer King",
+                    Descripcion = "Burger King, también conocida como BK, es una cadena de establecimientos de comida rápida estadounidense con sede central en Miami, fundada por James McLamore y David Edgerton, presente a nivel internacional y especializada principalmente en la elaboración de hamburguesas.",
+                    Imagen = "https://onlyfood.blob.core.windows.net/imagenes/bklogo%20-%20copia.png",
+                    Web = "https://www.burgerking.es/"
+                },
+                new Cadena()
+                {
+                    Id = 2,
+                    Nombre = "KFC",
+                    Descripcion = "Burger King, también conocida como BK, es una cadena de establecimientos de comida rápida estadounidense con sede central en Miami, fundada por James McLamore y David Edgerton, presente a nivel internacional y especializada principalmente en la elaboración de hamburguesas.",
+                    Imagen = "https://onlyfood.s3.amazonaws.com/kfc.png",
+                    Web = "https://www.burgerking.es/"
+                },
+                new Cadena()
+                {
+                    Id = 3,
+                    Nombre = "McDonald's",
+                    Descripcion = "Burger King, también conocida como BK, es una cadena de establecimientos de comida rápida estadounidense con sede central en Miami, fundada por James McLamore y David Edgerton, presente a nivel internacional y especializada principalmente en la elaboración de hamburguesas.",
+                    Imagen = "https://onlyfood.s3.amazonaws.com/McDonaldsEmblema.png",
+                    Web = "https://www.burgerking.es/"
+                },
+            };
+
+            this.Cadenas = new ObservableCollection<Cadena>(cadenasLocal);
+        }
+
+        public ObservableCollection<Cadena> Cadenas { get; set; }
+        private Cadena _CadenaSeleccionada;
+        public Cadena CadenaSeleccionada
+        {
+            get { return this._CadenaSeleccionada; }
+            set
+            {
+                this._CadenaSeleccionada = value;
+                // Solo usa el parametro si el command lo tiene declarado
+                this.MostrarOfertas.Execute(1);  
+                OnPropertyChanged("CadenaSeleccionada");
+            }
+        }
+
+        public Command MostrarOfertas
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    int idcadena = this.CadenaSeleccionada.Id;
+                    OfertasView view = new OfertasView();
+                    view.BindingContext = new OfertasViewModel(idcadena);
+                    Application.Current.MainPage.Navigation.PushModalAsync(view);
+                });
+            }
+        }
+    }
+}
---
+++ OnlyFoodXamarin/OnlyFoodXamarin/ViewModels/OfertasViewModel.cs
@@ -0,0 +1,73 @@
+﻿using OnlyFoodXamarin.Base;
+using OnlyFoodXamarin.Models;
+using OnlyFoodXamarin.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OnlyFoodXamarin.ViewModels
+{
+    public class OfertasViewModel : ViewModelBase
+    {
+        private ObservableCollection<Oferta> _Ofertas;
+        public ObservableCollection<Oferta> Ofertas
+        {
+            get { return this._Ofertas; }
+            set
+            {
+                this._Ofertas = value;
+                OnPropertyChanged("Ofertas");
+            }
+        }
+
+        public OfertasViewModel(int idcadena)
+        {
+            List<Oferta> ofertaslocal = new List<Oferta>()
+            {
+                new Oferta()
+                {
+                    Id = 1,
+                    Codigo = "D105",
+                    Descripcion="Cono de nata por 0,50€",
+                    IdCadena=1,
+                    IdUsuario=1,
+                    Imagen= "https://onlyfood.s3.amazonaws.com/longchickenbk.jpg",
+                    Precio=5.49,
+                    Titulo="Menu mediano: Hamburguesa long chicken con patatas y bebida medianas",
+                    Web= "https://www.burgerking.es/"
+                },
+                new Oferta()
+                {
+                    Id = 2,
+                    Codigo = "B110",
+                    Descripcion="Cono de nata por 0,50€",
+                    IdCadena=2,
+                    IdUsuario=1,
+                    Imagen= "https://onlyfood.s3.amazonaws.com/B110.jpg",
+                    Precio=24.99,
+                    Titulo="8 TIRAS DE PECHUGA + 6 PIEZAS DE POLLO + 4 BEBIDAS + 4 PATATAS por 24,99€",
+                    Web= "https://www.burgerking.es/"
+                },
+                new Oferta()
+                {
+                    Id = 3,
+                    Codigo = "D105",
+                    Descripcion="Cono de nata por 0,50€",
+                    IdCadena=3,
+                    IdUsuario=1,
+                    Imagen= "https://onlyfood.s3.amazonaws.com/cbo.jpg",
+                    Precio=5.45,
+                    Titulo="Menú CBO: Hamburguesa CBO, bebida y patatas pequeñas",
+                    Web= "https://www.burgerking.es/"
+                }
+            };
+
+            this.Ofertas = new ObservableCollection<Oferta>
+                (ofertaslocal.Where(x => x.IdCadena == idcadena));
+        }
+    }
+}
